0082746119687428fbd5e31ac13b61d5352f293a
Al-jhnme <52810715+aljhnme@users.noreply.github.com>
2020-07-06 07:40:50 -0700

    Add files via upload

---
+++ storage/framework/views/4119e55918de1b0a685982806b45c05100328471.php
@@ -0,0 +1,111 @@
+<html>
+ <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Laravel 5.8 - DataTables Server Side Processing using Ajax</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+  <div class="container">    
+     <br />
+     <h3 align="center">Dynamically Add / Remove input fields in Laravel 5.8 using Ajax jQuery</h3>
+     <br />
+   <div class="table-responsive">
+                <form method="post" id="dynamic_form_input">
+                 <span id="result"></span>
+                 <table class="table table-bordered table-striped" id="user_table">
+               <thead>
+                <tr>
+                    <th width="35%">First Name</th>
+                    <th width="35%">Last Name</th>
+                    <th width="30%">Action</th>
+                </tr>
+               </thead>
+               <tbody>
+
+               </tbody>
+               <tfoot>
+                <tr>
+                                <td colspan="2" align="right">&nbsp;</td>
+                                <td>
+                  <?php echo csrf_field(); ?>
+                  <input type="submit" name="save" id="save" class="btn btn-primary" value="Save" />
+                 </td>
+                </tr>
+               </tfoot>
+           </table>
+                </form>
+   </div>
+  </div>
+ </body>
+</html>
+
+<script>
+$(document).ready(function(){
+
+ var count = 1;
+
+ dynamic_input(count);
+
+ function dynamic_input(number)
+ {
+  html = '<tr>';
+        html += '<td><input type="text" name="first_name[]" class="form-control" /></td>';
+        html += '<td><input type="text" name="last_name[]" class="form-control" /></td>';
+        if(number > 1)
+        {
+            html += '<td><button type="button" name="remove" id="" class="btn btn-danger remove">Remove</button></td></tr>';
+            $('tbody').append(html);
+        }
+        else
+        {   
+            html += '<td><button type="button" name="add" id="add" class="btn btn-success">Add</button></td></tr>';
+            $('tbody').html(html);
+        }
+ }
+
+ $(document).on('click', '#add', function(){
+  count++;
+  dynamic_input(count);
+ });
+
+ $(document).on('click', '.remove', function(){
+  count--;
+  $(this).closest("tr").remove();
+ });
+
+ $('#dynamic_form_input').on('submit', function(event){
+        event.preventDefault();
+        $.ajax({
+            url:'<?php echo e(route("dynamic-field.insert_data")); ?>',
+            method:'post',
+            data:$(this).serialize(),
+            dataType:'json',
+            beforeSend:function(){
+                $('#save').attr('disabled','disabled');
+            },
+            success:function(data)
+            {
+                if(data.error)
+                {
+                    var error_html = '';
+                    for(var count = 0; count < data.error.length; count++)
+                    {
+                        error_html += '<p>'+data.error[count]+'</p>';
+                    }
+                    $('#result').html('<div class="alert alert-danger">'+error_html+'</div>');
+                }
+                else
+                {
+                    dynamic_input(1);
+                    $('#result').html('<div class="alert alert-success">'+data.success+'</div>');
+                }
+                $('#save').attr('disabled', false);
+            }
+        })
+ });
+
+});
+</script>
+<?php /**PATH C:\xam\htdocs\treval\resources\views/dynamic_field.blade.php ENDPATH**/ ?>
